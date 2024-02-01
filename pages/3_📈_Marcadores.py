04291ead6670c06d3283a1a30e77edc59b06c49b
Aloisio <aloisiovilasboas@gmail.com>
2024-02-01 01:28:46 -0300

    streamlit marcadores1

--- pages/3_📈_Marcadores.py
+++ pages/3_📈_Marcadores.py
@@ -1,60 +1,61 @@
 import streamlit as st
 from streamlit_tags import st_tags, st_tags_sidebar
+import pandas as pd
 
 
-
-
-
-
-marcadoresDefault = ['Deso', 'Energisa', 'Two']
- 
+# Inicializa o datastream se ele não existir
+if 'filtros' not in st.session_state:
+    st.session_state['filtros'] = []
+if 'marcadoresDosFiltros' not in st.session_state:
+    st.session_state['marcadoresDosFiltros'] = []
 
 if 'marcadores' not in st.session_state:
-    marcadoresValue = marcadoresDefault
-else:
-    marcadoresValue = st.session_state.marcadores
+    st.session_state['marcadores'] = []
 
-title = st.text_input('Movie title', 'Life of Brian')
-st.write('The current movie title is', title)
+    
 
+col1, col2 = st.columns(2)
 
-marcadoresNaTela = st_tags(
-    label='# Enter Keywords:',
-    text='Press enter to add more',
-    value=marcadoresValue,
-    suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
-    maxtags=20,
-    key="marcadoresNaTela")
+with col1:
+    with st.form("my_form", clear_on_submit=True):
+        st.header("Filtros")
+        # Entrada de texto para adicionar uma nova palavra
+        novo_filtro = st.text_input('Insira um novo filtro')
+        options = st.multiselect(
+            'Aplicar marcadores',
+            st.session_state['marcadores'],
+            [])
 
-st.session_state.marcadores = marcadoresNaTela
-
-
-if 'a_counter' not in st.session_state:
-    st.session_state.a_counter = 0
-
-st.write("### Counter:")
-st.write(st.session_state.a_counter)
-
-button = st.button("Increment Counter")
-if button:
-    st.session_state.a_counter += 1
-    "apertou",st.session_state.a_counter
+            # Botão para adicionar a nova palavra ao datastream
+        
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.session_state['filtros'].append(novo_filtro)
+            st.session_state['marcadoresDosFiltros'].append(options)
+# Exibe o datastream atualizado
+    dff = pd.DataFrame({'filtros':st.session_state['filtros'], 'marcadoresDosFiltros':st.session_state['marcadoresDosFiltros']})
+    st.data_editor(dff, hide_index=True, num_rows = 'dynamic', disabled=['filtros', 'marcadoresDosFiltros'])
 
 
-st.write("### Session State:")
-st.write(st.session_state)
+#CRUD Filtros
+   
+
+with col2:
+    with st.form("my_formarcadores", clear_on_submit=True):
+        st.header("Marcadores")
+        # Entrada de texto para adicionar uma nova palavra
+        novo_marcador = st.text_input('Insira um novo marcador')
+
+            # Botão para adicionar a nova palavra ao datastream
+        
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.session_state['marcadores'].append(novo_marcador)
+            st.rerun()
+# Exibe o datastream atualizado
+    dfm = pd.DataFrame(st.session_state['marcadores'], columns=['nome'])
+    st.write(edited_dfm = st.data_editor(dfm, hide_index=True,key="dfm"))
 
 
-maxtags_sidebar = st.sidebar.slider('Number of tags allowed?', 1, 10, 3, key='1')
 
-
-keyword = st_tags_sidebar(label='# Enter Keywords:',
-                          text='Press enter to add more',
-                          value=['Zero', 'One', 'Two'],
-                          suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
-                          maxtags=maxtags_sidebar,
-                          key="afrfae")
-
-st.sidebar.write("### Results:")
-st.sidebar.write((keyword))
 
